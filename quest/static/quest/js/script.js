1166382a5a8ac815563f1ef54926b4fefcd0c927
awkwardio <mrmindshattering@gmail.com>
2018-05-23 14:49:41 -0400

    removed static folder inside quest

--- quest/static/quest/js/script.js
+++
@@ -1,6 +0,0 @@
-function wronganswer() {
-	alert("Sorry. You picked the wrong chalice! Try again.")
-}
-function rightanswer() {
-	alert("The king is saved! You picked the right chalice. Congratulations!")
-}
