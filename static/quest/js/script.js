df3be79763996a8e20b691bc5407ed0b4b170f64
awkwardio <mrmindshattering@gmail.com>
2018-05-23 14:47:17 -0400

    fixed the static files

---
+++ static/quest/js/script.js
@@ -0,0 +1,6 @@
+function wronganswer() {
+	alert("Sorry. You picked the wrong chalice! Try again.")
+}
+function rightanswer() {
+	alert("The king is saved! You picked the right chalice. Congratulations!")
+}
