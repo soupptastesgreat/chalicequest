200761e15eff37c9eba8f5fd841e06bc6bc477ab
Christopher Thomas <mrmindshattering@gmail.com>
2018-05-23 16:43:53 -0400

    Update script.js

--- static/quest/js/script.js
+++ static/quest/js/script.js
@@ -1,5 +1,5 @@
 function wronganswer() {
-	alert("Sorry. You picked the wrong chalice! Try again.")
+	alert("Sorry, you picked the wrong chalice! Try again.")
 }
 function rightanswer() {
 	alert("The king is saved! You picked the right chalice. Congratulations!")
